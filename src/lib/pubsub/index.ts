24030cc81494ce4c674aac2c93014b4cd426d9e7
Mats Larsen <mats@jun.codes>
2024-10-12 16:10:15 +0900

    Nuke redis

--- src/lib/pubsub/index.ts
+++ src/lib/pubsub/index.ts
@@ -1,23 +1,3 @@
-import { RedisPubSub } from 'graphql-redis-subscriptions';
 import { PubSub } from 'graphql-subscriptions';
-import Redis from 'ioredis';
 
-const options: Redis.RedisOptions = {
-    host: process.env.REDIS_HOST,
-    port: 6379,
-    tls: {
-        servername: process.env.REDIS_HOST,
-    },
-    retryStrategy: (times: number) => {
-        // reconnect after
-        return Math.min(times * 50, 2000);
-    },
-};
-
-export const pubsub =
-    process.env.NODE_ENV == 'production'
-        ? new RedisPubSub({
-              publisher: new Redis(options),
-              subscriber: new Redis(options),
-          })
-        : new PubSub();
+export const pubsub = new PubSub();
